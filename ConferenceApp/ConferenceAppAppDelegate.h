f939dc2ccc837392425275b97ca4310bd6b475a9
Peter Friese <peter@peterfriese.de>
2011-09-21 08:09:23 +0200

    Initial Commit

---
+++ ConferenceApp/ConferenceAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ConferenceAppAppDelegate.h
+//  ConferenceApp
+//
+//  Created by Peter Friese on 21.09.11.
+//  Copyright 2011 http://peterfriese.de. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConferenceAppAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ ConferenceApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ConferenceApp
+//
+//  Created by Peter Friese on 21.09.11.
+//  Copyright 2011 http://peterfriese.de. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
