2ddaa6b5016fbe36dae8014a8ce784943d2e19bb
Peter Friese <peter@peterfriese.de>
2011-10-06 15:51:49 +0200

    Styled section header for SpeakerTableView.

--- Source/Speaker/SpeakerTableViewController.m
+++ Source/Speaker/SpeakerTableViewController.m
@@ -33,6 +33,34 @@
     return [[self fetchedResultsControllerForTableView:tableView] sectionIndexTitles];
 }
 
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    NSString *label = @"";
+    NSArray *sections = [[self fetchedResultsControllerForTableView:tableView] sections];
+    if ([sections count]) {
+        id<NSFetchedResultsSectionInfo> sectionInfo = [sections objectAtIndex:section];
+        Speaker *speaker = (Speaker *)[[sectionInfo objects] objectAtIndex:0];
+        label = [speaker initial];
+    }
+    
+    
+    UIView* customView = [[[UIView alloc] initWithFrame:CGRectMake(10.0, 0.0, 300.0, 44.0)] autorelease];
+    customView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"section_grained_opa90"]];
+    
+    UILabel * headerLabel = [[[UILabel alloc] initWithFrame:CGRectZero] autorelease];
+    headerLabel.backgroundColor = [UIColor clearColor];
+    headerLabel.opaque = NO;
+    headerLabel.textColor = [UIColor whiteColor];
+    headerLabel.font = [UIFont boldSystemFontOfSize:18];
+    headerLabel.shadowOffset = CGSizeMake(0.0f, 1.0f);
+    headerLabel.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5];
+    headerLabel.frame = CGRectMake(11,-11, 320.0, 44.0);
+    headerLabel.textAlignment = UITextAlignmentLeft;
+    headerLabel.text = label;
+    [customView addSubview:headerLabel];
+    return customView;    
+}
+
 -(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
     NSArray *sections = [[self fetchedResultsControllerForTableView:tableView] sections];
